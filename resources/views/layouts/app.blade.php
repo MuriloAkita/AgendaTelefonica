135a7e73e303a4cfbc043eeeea1d1df1efb393b9
MuriloAkita <murilo.akita@gmail.com>
2022-11-27 13:43:20 -0300

    Added assets from template :sparkles:

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.layout')
+
+@section('content')
+aa
+    {{-- Conteúdo --}}
+    <div class="page-body">
+        @yield('content_header')
+        @yield('content_body')
+    </div>
+@endsection
