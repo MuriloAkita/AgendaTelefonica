fd1f2278ce1f6ebf9d1ec2a1734cabaa1b6332c5
MuriloAkita <murilo.akita@gmail.com>
2022-11-30 10:47:49 -0300

    Datatables translate :sparkles:

--- resources/views/agenda/index.blade.php
+++ resources/views/agenda/index.blade.php
@@ -4,13 +4,11 @@
 @endsection
 
 @section('content_body')
-    <div class="container">
-        <div class="mb-5 ml-2">
-            <a class="btn btn-primary" href="{{ route('agenda.create') }}">Cadastrar</a>
-        </div>
+    <div class="mb-5 ml-2">
+        <a class="btn btn-primary" href="{{ route('agenda.create') }}">Cadastrar</a>
+    </div>
 
-        <div class="col-md-12">
-            <livewire:agendas />
-        </div>
+    <div class="col-md-12">
+        <livewire:agendas />
     </div>
 @endsection
